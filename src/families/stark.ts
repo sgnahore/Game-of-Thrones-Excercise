80a89d9d049713dc1777bd1ded223475566128c8
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-24 10:24:09 +0100

    added Stark family tree

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,23 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
+  const eddardKids: Person[] = [
+    { name: "Rickon", children: [] },
+    { name: "Bran", children: [] },
+    { name: "Arya", children: [] },
+    { name: "Sansa", children: [] },
+    { name: "Robb", children: [] },
+    { name: "Jon", children: [] },
+  ];
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+  const richardKids: Person[] = [
+    { name: "Benjen", children: [] },
+    { name: "Lyanna", children: [] },
+    { name: "Eddard", children: eddardKids },
+    { name: "Brandon", children: [] },
+  ];
+
+  const topPerson: Person = { name: "Richard", children: richardKids };
+
+  return topPerson;
 }
