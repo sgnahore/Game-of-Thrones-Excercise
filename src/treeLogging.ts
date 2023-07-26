afe1c9398d056c7f46c4653816580b15882f956b
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-26 11:18:10 +0100

    started adding logAllPeopleInTree function implementation

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,7 +1,23 @@
 import { Person } from "./personTypes";
 
+/**function logAllPeopleInTree(rootPerson)
+    workStack = empty stack of Person objects
+    push rootPerson onto workStack
+
+		while workStack is not empty: 
+        currentPerson = pop person off workStack
+        log the name property of currentPerson
+        push each element of currentPerson's children onto the workStack
+         */
+
 export function logAllPeopleInTree(topPerson: Person): void {
-  console.log("TODO!  First person is " + topPerson.name);
+  let workStack: Person[] = [];
+  workStack.push(topPerson);
+  console.log("workstack is", workStack);
+
+  while (workStack.length > 0) {}
+
+  // console.log("TODO!  First person is " + topPerson.name);
 }
 
 export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
